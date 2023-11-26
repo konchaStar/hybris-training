1d9a6e652a308b7f0b0956322aa61f6b33092ac4
maksim <maks.volontirov@gmail.com>
2023-11-26 17:40:26 +0300

    task 2 Int tests for BandDao

--- bin/custom/concerttours/src/concerttours/daos/impl/DefaultBandDao.java
+++ bin/custom/concerttours/src/concerttours/daos/impl/DefaultBandDao.java
@@ -4,14 +4,18 @@ import concerttours.daos.BandDao;
 import concerttours.model.BandModel;
 import de.hybris.platform.servicelayer.search.FlexibleSearchQuery;
 import de.hybris.platform.servicelayer.search.FlexibleSearchService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component("bandDao")
 public class DefaultBandDao implements BandDao {
     private static final String SELECT_ALL_QUERY = "SELECT {p:" + BandModel.PK + "} FROM {" + BandModel._TYPECODE + " AS p} ";
     private static final String SELECT_CODE_QUERY = "SELECT {p:" + BandModel.PK + "} FROM {" + BandModel._TYPECODE + " AS p} "
             + "WHERE " + "{p:" + BandModel.CODE + "}=?code ";
     private static final String CODE_PARAM = "code";
+    @Autowired
     private FlexibleSearchService flexibleSearchService;
 
     @Override
