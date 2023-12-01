8b82aab9b712fbb8609621afcaa9443ad1df50ee
maksim <maks.volontirov@gmail.com>
2023-12-01 20:18:02 +0300

    Task 3: Add days until concert dynamic attribute

---
+++ bin/custom/concerttours/src/concerttours/dynamicattributeshandlers/DaysUntilConcertDynamicAttributeHandler.java
@@ -0,0 +1,28 @@
+package concerttours.dynamicattributeshandlers;
+
+import concerttours.model.ConcertModel;
+import de.hybris.platform.servicelayer.model.attribute.AbstractDynamicAttributeHandler;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Nonnull;
+import java.time.Duration;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Optional;
+
+@Component
+public class DaysUntilConcertDynamicAttributeHandler extends AbstractDynamicAttributeHandler<Optional<Long>, ConcertModel> {
+    @Override
+    public Optional<Long> get(@Nonnull ConcertModel model) {
+        if (model.getDate() == null) {
+            return Optional.empty();
+        }
+
+        ZonedDateTime date = model.getDate().toInstant().atZone(ZoneId.systemDefault());
+        ZonedDateTime now = ZonedDateTime.now();
+        Duration duration = Duration.between(now, date);
+
+        return Optional.of(duration.toDays());
+    }
+
+}
